4dbbbd4ad507a92e6bd492a07da85ef4b32f1bcc
dgarcia <alejandrogarcia-20@hotmail.com>
2023-07-11 21:23:57 -0300

    Se agrega la opción de borrar un producto del carrito

--- js/app.js
+++ js/app.js
@@ -98,29 +98,34 @@ productos.push(new Producto("Piedra aglomerantes", 580, 1));
 productos.push(new Producto("Prednisolona", 120, 1));
 productos.push(new Producto("Shampoo Dominal", 280, 1));
 
-localStorage.setItem('stock', JSON.stringify(productos)); 
+localStorage.setItem('stock', JSON.stringify(productos));
 
 let stock = [];
 let carrito = [];
 let tabla = [];
-const total = document.getElementById('total');
-const bodyTabla = document.getElementById('items');
+const total = document.getElementById('total'); // fila que va registrar el total de carrito
+const bodyTabla = document.getElementById('items'); // cuerpo de la tabla que se va agregar filas del carrrito
+/*const btnRegistrar = document.getElementById('registrarUsuario') // obtengo boton de registro usuario
+let portada = document.getElementById('header');
+localStorage.setItem('portada', portada);*/
+
 
 function traerItems() {
     stock = JSON.parse(localStorage.getItem('stock')) || [];
     carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+    //portada = JSON.parse(localStorage.getItem('portada')) || [];
 }
 
 
 function agregarCarrito(i) {
-    const indexCarrito = carrito.findIndex((item) => item.nombre === stock[i-1].nombre);
+    const indexCarrito = carrito.findIndex((item) => item.nombre === stock[i - 1].nombre);
     let n;
     if (indexCarrito !== -1) {
         carrito[indexCarrito].cantidad++;
         n = indexCarrito;
     }
     else {
-        n = carrito.push(stock[i-1]);
+        n = carrito.push(stock[i - 1]);
         n--;
     }
     localStorage.setItem('carrito', JSON.stringify(carrito));
@@ -133,59 +138,115 @@ function allEventListeners() {
         e.preventDefault();
         agregarCarrito(1);
         dibujarTabla();
+        alert("Producto agregado al carrito");
     });
     document.getElementById("btn_2").addEventListener('click', (e) => {
         e.preventDefault();
         agregarCarrito(2);
         dibujarTabla();
+        alert("Producto agregado al carrito");
     });
     document.getElementById("btn_3").addEventListener('click', (e) => {
         e.preventDefault();
         agregarCarrito(3);
         dibujarTabla();
+        alert("Producto agregado al carrito");
     });
     document.getElementById("btn_4").addEventListener('click', (e) => {
         e.preventDefault();
         agregarCarrito(4);
         dibujarTabla();
+        alert("Producto agregado al carrito");
     });
     document.getElementById("btn_5").addEventListener('click', (e) => {
         e.preventDefault();
         agregarCarrito(5);
         dibujarTabla();
+        alert("Producto agregado al carrito");
     });
     document.getElementById("btn_6").addEventListener('click', (e) => {
         e.preventDefault();
         agregarCarrito(6);
         dibujarTabla();
+        alert("Producto agregado al carrito");
     });
     document.getElementById("btn_7").addEventListener('click', (e) => {
         e.preventDefault();
         agregarCarrito(7);
         dibujarTabla();
+        alert("Producto agregado al carrito");
     });
     document.getElementById("btn_8").addEventListener('click', (e) => {
         e.preventDefault();
         agregarCarrito(8);
         dibujarTabla();
+        alert("Producto agregado al carrito");
     });
+
+    /*btnRegistrar.addEventListener('click', (e) => {
+        e.preventDefault();
+        const nom = document.querySelector('#inputNomUsu').value;
+        const ape = document.querySelector('#inputApeUsu').value;
+        localStorage.setItem("apellido", ape);
+        localStorage.setItem("nombre", nom);
+        modificarPortada();
+        alert("Usuario Registrado");
+    });*/
 }
 
 
 function dibujarTabla() {
     bodyTabla.innerHTML = ``; //me aseguro que la tabla no contenga datos
     total.innerText = 0;
-    carrito.forEach((item,index) => {
-        bodyTabla.innerHTML = bodyTabla.innerHTML + 
-                    `<tr>
-                        <th scope="row">${index+1}</th>
-                        <td>${item.nombre}</td>
-                        <td>${item.precio}</td>
-                        <td>${item.cantidad}</td>
-                    </tr>
-        `;
+    carrito.forEach((item, index) => {
+        nuevaFila(item, index);
     });
-    total.textContent = carrito.reduce((acc, item) => acc + (item.precio * item.cantidad), 0); 
+}
+
+function nuevaFila(item, index) {
+    const row = document.createElement('tr'); ///creo la fila
+
+    let td = document.createElement('td');
+    td.textContent = index + 1;
+    row.appendChild(td);
+
+    td = document.createElement('td');
+    td.textContent = item.nombre;
+    row.appendChild(td);
+
+    td = document.createElement('td');
+    td.textContent = item.precio;
+    row.appendChild(td);
+
+
+    td = document.createElement('td');
+    td.textContent = item.cantidad;
+    row.appendChild(td);
+
+
+
+    ///agrego una nueva celda con el boton eliminar
+    td = document.createElement('td');
+    const btnEliminar = document.createElement('button');
+    btnEliminar.classList.add('btn', 'btn-danger');
+    btnEliminar.textContent = 'Borrar';
+
+    ///cuando hace click se debe eliminar del CARRITO el elemento
+    // actualizar el localstorage y actualizar la tabla
+    btnEliminar.onclick = () => {
+        carrito.splice(index, 1); ///le envio el indice y la cantidad de 1 (porque voy a borrar solo 1)
+        dibujarTabla();
+        localStorage.setItem('carrito', JSON.stringify(carrito));
+    }
+
+    td.appendChild(btnEliminar);
+    row.appendChild(td);
+    bodyTabla.appendChild(row);
+
+    ////calculo el total que tengo ahora
+    total.textContent = carrito.reduce((acc, item) => acc + item.precio * item.cantidad, 0);
 }
 
 allEventListeners();
+
+// regex
